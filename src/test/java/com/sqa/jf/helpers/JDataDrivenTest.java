de3842aa8820642bfd7df91165a43b210f268477
Jean-francois Nepton <sqasolution@hotmail.com>
2016-06-04 16:19:14 -0700

    Create JUnit Data Driven Test Template

---
+++ src/test/java/com/sqa/jf/helpers/JDataDrivenTest.java
@@ -0,0 +1,36 @@
+/**
+ *   File Name: JDataDrivenTest.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 1, 2016
+ *   
+ */
+
+package com.sqa.jf.helpers;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/**
+ * JDataDrivenTest //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      LastName, FirstName
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class JDataDrivenTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
